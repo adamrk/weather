5494396abcb2a8252a8482db24904674857d5c27
adamrk <ark.email@gmail.com>
2016-09-18 23:26:21 -0400

    completing db setup

--- db.py
+++ db.py
@@ -5,13 +5,17 @@ from sqlalchemy import ( create_engine,
                          String, 
                          DateTime,
                          Date,
-                         ForeignKey,)
-from sqlalchemy.orm import relationship
+                         ForeignKey,
+                         )
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-engine = create_engine('sqlite:///' + os.environ['DB_PATH'])
 Base = declarative_base()
+engine = create_engine('sqlite:///' + os.environ['DB_PATH'])
+# print "db path:  "
+# print 'sqlite:///' + os.environ['DB_PATH']
+
 
 class Crag(Base):
     __tablename__ = 'crags'
@@ -21,11 +25,14 @@ class Crag(Base):
     ## Note lat and lng are scaled by 100 e.g. 75.65 lat stored as 7565
     lat = Column(Integer)
     lng = Column(Integer)
+    wu_name = Column(String)
 
     forecasts = relationship("Forecast", back_populates='crag')
+    actuals = relationship("Actual", back_populates='crag')
 
     def __repr__(self):
-        return "<Crag(name=%s, lat=%d, lng=%d)>" % (self.name, self.lat, self.lng)
+        return "<Crag(name=%s, lat=%d, lng=%d, wu_name=%s)>" % (
+            self.name, self.lat, self.lng, self.wu_name)
 
 class Forecast(Base):
     __tablename__ = 'forecasts'
@@ -45,7 +52,35 @@ class Forecast(Base):
             self.service, self.crag_id, self.temp, 
             self.rain, self.pred_for.ascftime("%b %d %y"))
 
+class Actual(Base):
+    __tablename__ = 'actuals'
+
+    id = Column(Integer, primary_key=True)
+    temp = Column(Integer)
+    rain = Column(Integer)
+    date = Column(Date)
+    crag_id = Column(Integer, ForeignKey('crags.id'))
+
+    crag = relationship("Crag", back_populates="actuals")
+
+    def __repr__(self):
+        return "<Actual(crag=%s, temp=%d, rain=%s, date=%s>" % (
+            self.crag_id, self.temp, self.rain, 
+            self.date.ascftime("%b %d %y"))
+
 if __name__ == "__main__":
-    Base.metadata.create_all(engine)        
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    crags = [
+        Crag(name="Gunks", lat=4174, lng=-7408, wu_name="NY/New_Paltz"),
+        Crag(name="Red River Gorge", lat=3779, lng=-8370, wu_name="KY/Slade")
+        ]
+    for crag in crags:
+        session.add(crag)
+    session.commit()
+
+
+
 
 
